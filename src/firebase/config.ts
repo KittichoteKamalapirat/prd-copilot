6310b89c2a0e88554a565aa4626b1ad5ae3e4052
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2024-12-01 19:54:00 +0700

    chore: disable analytics

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -20,6 +20,6 @@ const firestore = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
-export { app, firestore, auth, storage, analytics };
+export { app, firestore, auth, storage };
