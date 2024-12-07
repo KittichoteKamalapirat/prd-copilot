7d20d638af6ed39e5d582938392285a4a7f52a6f
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2024-12-07 11:22:02 +0700

    chore: setup zustand

---
+++ src/lib/store.ts
@@ -0,0 +1,82 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { unflatten } from "flat";
+import { create } from "zustand";
+
+export type IStoreSet<Props = Record<string, any>> = (
+  keyOrObj: Partial<Props> | keyof Props,
+  value?: any,
+) => void;
+
+type PropsWithSet<Props = Record<string, any>> = Props & {
+  set: IStoreSet<Props>;
+};
+
+interface DisplayStore {
+  showUpsellSheet: boolean;
+}
+
+export type IStore = {
+  display: PropsWithSet<DisplayStore>;
+};
+
+export const useStore = create<IStore>((set, get) => {
+  function factorySetFunc<ContextObject = IStore>(
+    prefixPath: keyof IStore,
+    cb?: () => void,
+  ) {
+    const _set: IStoreSet<Partial<Omit<ContextObject, "set">>> = (
+      keyOrObj,
+      value,
+    ) => {
+      const state = get();
+      const stateInContext = state[prefixPath];
+
+      let flattenedObject: Record<string, any>;
+      const isFirstParamObject =
+        (keyOrObj as Record<string, any>)?.constructor?.name === "Object";
+      if (isFirstParamObject) {
+        flattenedObject = {
+          [prefixPath]: {
+            ...stateInContext,
+            ...(keyOrObj as Record<string, any>),
+          },
+        };
+      } else {
+        if (value === undefined) {
+          throw new Error(
+            `set/${prefixPath}/${
+              keyOrObj as string
+            }: property name was provided but value is missing. Provide the value to be set.`,
+          );
+        }
+        const keyPath = `${keyOrObj as string}`.replace(/\.+/, ".");
+        flattenedObject = {
+          [prefixPath]: { ...stateInContext, [keyPath]: value },
+        };
+      }
+
+      const unflattenedObject = unflatten(flattenedObject);
+      set(unflattenedObject as IStore);
+      cb?.();
+    };
+
+    return _set;
+  }
+
+  const store: IStore = {
+    display: {
+      set: factorySetFunc<IStore["display"]>("display"),
+      showUpsellSheet: false,
+    },
+  };
+
+  return store;
+});
+
+// useStore.getState() basically get new reference of cloned state
+export const initialStoreState = useStore.getState();
+
+export const resetStoreState = () =>
+  useStore.setState({
+    ...initialStoreState,
+  });
