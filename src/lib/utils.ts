4445ac39fc2dde899daa251e46a7c1b906ceb395
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2024-12-13 18:20:43 +0700

    chore: add object keys values type utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+export type ObjectKeys<T> = keyof T;
+export type ObjectValues<T> = T[keyof T];
