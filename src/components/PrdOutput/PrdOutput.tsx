57ab68d4c4d69d2feb0c0e7cb5b8eba9f03ff5a3
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2024-12-14 10:55:49 +0700

    feat: add lock icon

--- src/components/PrdOutput/PrdOutput.tsx
+++ src/components/PrdOutput/PrdOutput.tsx
@@ -5,6 +5,7 @@ import { useStore } from '../../lib/store'
 import './PrdOutput.css'
 import { cn } from '@/lib/utils'
 import { Button } from '../ui/button'
+import { Lock } from 'lucide-react'
 
 interface Props {
   isAuth?: boolean
@@ -28,7 +29,7 @@ export const PrdOutput = ({ isAuth, isPro }: Props) => {
     if (!isPro) set({ showUpsellSheet: true })
   }
   return (
-    <div className="form-textarea relative w-full p-4 px-8 overflow-scroll h-[calc(100%-1rem)] shadow-2xl flex items-center justify-center">
+    <div className="form-textarea relative w-full p-4 px-8 overflow-scroll h-[calc(100%-1rem)] shadow-2xl flex items-center justify-center select-none">
       <div className="text-left absolute top-4 left-4">
         <div className={cn('is-typed', isBlurred && 'blurred')}>
           <ReactMarkdown>{text || placeHolder}</ReactMarkdown>
@@ -38,6 +39,7 @@ export const PrdOutput = ({ isAuth, isPro }: Props) => {
       {text && !isPro && (
         <div className="sticky top-1/2 transform">
           <Button onClick={handleInvalidUser} size="lg" className="">
+            <Lock />
             {blockLabel}
           </Button>
         </div>
