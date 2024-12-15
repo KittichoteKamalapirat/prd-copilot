c2b176365a892664f320ab4a44aef6c4897bdbd5
kittichote-kamalapirat-ah <kittichote.kamalapirat@earnin.com>
2024-12-15 21:05:26 +0700

    fix: page params next error

--- src/app/app/prds/[id]/page.tsx
+++ src/app/app/prds/[id]/page.tsx
@@ -5,7 +5,12 @@ import { getTokens } from 'next-firebase-auth-edge'
 import { cookies } from 'next/headers'
 import PrdDetailPage from '../../../PrdDetailPage'
 
-export default async function PRDDetailPage({ params }: { params: { id: string } }) {
+type Params = Promise<{ id: string }> // ref: https://stackoverflow.com/questions/79113322/nextjs-react-type-does-not-satisfy-constraint
+
+export default async function PRDDetailPage(props: { params: Params }) {
+  const params = await props.params
+  const prdId = params.id
+
   const tokens = await getTokens(await cookies(), {
     apiKey: clientConfig.apiKey,
     cookieName: serverConfig.cookieName,
@@ -19,5 +24,5 @@ export default async function PRDDetailPage({ params }: { params: { id: string }
 
   const userId = tokens.decodedToken.uid
 
-  return <PrdDetailPage userId={userId} prdId={params.id} />
+  return <PrdDetailPage userId={userId} prdId={prdId} />
 }
